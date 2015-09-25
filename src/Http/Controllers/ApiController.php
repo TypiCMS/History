e68358646fcefe3c91c2a4e58fbdcf3960e6688c
Samuel De Backer <sdebacker@gmail.com>
2015-09-25 19:42:50 +0200

    Api FormRequest

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -1,6 +1,7 @@
 <?php
 namespace TypiCMS\Modules\History\Http\Controllers;
 
+use Illuminate\Support\Facades\Input;
 use TypiCMS\Modules\Core\Http\Controllers\BaseApiController;
 use TypiCMS\Modules\History\Repositories\HistoryInterface as Repository;
 
@@ -20,5 +21,26 @@ class ApiController extends BaseApiController
     {
         $models = $this->repository->latest(25, ['historable', 'user'], true);
         return response()->json($models, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Model|false
+     */
+    public function store()
+    {
+        return $this->repository->create(Input::all());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  $model
+     * @return boolean
+     */
+    public function update($model)
+    {
+        return $this->repository->update(Input::all());
     }
 }
