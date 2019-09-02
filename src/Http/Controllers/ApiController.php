173fca32b8ffeb1bbb5cfe752b6d6daf4feb9471
Samuel De Backer <sdebacker@gmail.com>
2019-09-02 15:05:07 +0200

    return type declarations

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -2,19 +2,16 @@
 
 namespace TypiCMS\Modules\History\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Spatie\QueryBuilder\QueryBuilder;
 use TypiCMS\Modules\Core\Http\Controllers\BaseApiController;
 use TypiCMS\Modules\History\Models\History;
 
 class ApiController extends BaseApiController
 {
-    public function __construct(Repository $repository)
-    {
-        parent::__construct($repository);
-    }
-
-    public function index(Request $request)
+    public function index(Request $request): LengthAwarePaginator
     {
         $data = QueryBuilder::for(History::class)
             ->paginate($request->input('per_page'));
@@ -22,7 +19,7 @@ class ApiController extends BaseApiController
         return $data;
     }
 
-    public function destroy()
+    public function destroy(): JsonResponse
     {
         $cleared = $this->model->clear();
 
