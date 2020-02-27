02e0ca3b72e07963ea1a1dadfbe083c0c29240f6
Samuel De Backer <sdebacker@gmail.com>
2020-02-27 14:57:09 +0000

    Apply fixes from StyleCI

--- src/Models/History.php
+++ src/Models/History.php
@@ -14,6 +14,7 @@ class History extends Base
     use PresentableTrait;
 
     protected $table = 'history';
+
     protected $presenter = ModulePresenter::class;
 
     protected $guarded = ['id', 'exit'];
